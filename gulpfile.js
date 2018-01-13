63c2aa5bb40e983a5902cde40034ec969a49ce58
Vitebo <vitebo51@gmail.com>
2018-01-13 21:55:11 -0200

    Add jade/pug

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,12 @@
 var gulp = require( 'gulp' );
+var pug = require( 'gulp-pug' );
+
+gulp.task( 'views', function buildHTML() {
+  return gulp.src( './src/*.pug' )
+             .pipe( pug({
+               opts: {
+                client: false
+               }
+             }) )
+             .pipe( gulp.dest( './src' ) )
+} );
