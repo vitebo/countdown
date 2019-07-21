62eb223daa61379d82fe044f922e506f6c8ea8a7
vitebo <vitebo@hotmail.com>
2019-07-21 15:30:20 -0300

    add html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -15,4 +16,9 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.pug'
+    }),
+  ]
 };
