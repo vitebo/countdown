60a98496ed632d035dd6d13fc48f461aa56acb3f
vitebo <vitebo@hotmail.com>
2019-11-17 15:26:29 -0300

    remove gh-pages devDependencies

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-docker run -it -p "8080:8080" -v "$(pwd):/var/www" -w "/var/www" node:12.6.0-alpine /bin/sh
+docker run \
+  -it \
+  -p "8080:8080" \
+  -v "$(pwd):/var/www/" \
+  -v "$(pwd)/node_modules/:/var/www/node_modules/" \
+  -w "/var/www/" \
+  node:12.6.0-alpine \
+  /bin/sh
